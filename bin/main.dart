8a5baf9cb44cea0041bb39efc6adba60ca501ec2
cristianfchq <cristian.fchq@gmail.com>
2019-08-18 19:25:17 -0400

    primer programa HOLA MUNDO

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+main(){
+  var a = 1;
+  print('HOLA MUNDO');
+
+  printNumber(a);
+}
+
+printNumber(int number){
+  print('el numero es: ${number}');
+
+  print('el numero es: ' + number.toString());
+}
